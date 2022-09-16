5edf1b7b442c8abc8bfd4074fd575c105bb4f73d
Dennis  Gikonyo <Dennis.Gikonyo@jubileekenya.com>
2022-09-16 15:08:27 +0300

    added laravel ui

---
+++ resources/js/App.vue
@@ -0,0 +1,21 @@
+<script>
+export default {};
+</script>
+<template>
+    <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="collapse navbar-collapse">
+                <div class="navbar-nav">
+                    <router-link to="/" class="nav-item nav-link"
+                        >Products List</router-link
+                    >
+                    <router-link to="/create" class="nav-item nav-link">
+                        >Create Product</router-link
+                    >
+                </div>
+            </div>
+        </nav>
+        <router-view></router-view>
+    </div>
+</template>
+https://www.laravelcode.com/post/laravel-8-crud-operation-with-vuejs
